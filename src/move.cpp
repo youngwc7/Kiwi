8253fff067061864e24224468f3c9489566cd588
Youngwoo Chang <youngwc7@uci.edu>
2026-07-06 18:53:14 +0900

    fixed bit shift issue when decoding 32 bit encoded move

--- src/move.cpp
+++ src/move.cpp
@@ -2,36 +2,36 @@
 
 int Move::getSourceSquare() const 
 {
-    return move & SOURCE_SQUARE_MASK;
+    return (move & SOURCE_SQUARE_MASK) // >> SOURCE_SQUARE_BIT_LSB;
 }
 
 int Move::getDestSquare() const 
 {
-    return move & DEST_SQUARE_MASK;
+    return (move & DEST_SQUARE_MASK) >> DEST_SQUARE_BIT_LSB;
 }
 
 bool Move::isEnPassant() const 
 {
-    return move & ENPASSANT_MASK;
+    return move & ENPASSANT_MASK // >> ENPASSANT_BIT_LSB;
 }
 
 PieceType Move::getPromotionPiece() const 
 {
-    return static_cast<PieceType>(move & PROMOTION_MASK);
+    return static_cast<PieceType>((move & PROMOTION_MASK) >> PROMOTION_BIT_LSB);
 }
 
 CastleMove Move::getCastleMove() const
 {
-    return static_cast<CastleMove>(move & CASTLE_MASK);
+    return static_cast<CastleMove>((move & CASTLE_MASK) >> CASTLE_BIT_LSB);
 }
 
 PieceType Move::getCapturedPiece() const 
 {
-    return static_cast<PieceType>(move & CAPTURED_PIECE_MASK);
+    return static_cast<PieceType>((move & CAPTURED_PIECE_MASK) >> CAPTURED_PIECE_LSB);
 }
 
 PieceType Move::getMovingPiece() const 
 {
-    return static_cast<PieceType>(move & MOVING_PIECE_MASK);
+    return static_cast<PieceType>((move & MOVING_PIECE_MASK) >> MOVING_PIECE_LSB);
 }
 
