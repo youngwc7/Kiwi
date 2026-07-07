d49454e2baa281df509fbe5c06a9d69dbcca614f
Youngwoo Chang <youngwc7@uci.edu>
2026-07-07 21:55:27 +0900

    added attack_map (precomputed movement tables/sliding rays) struct declaration

---
+++ src/attack_map.cpp
@@ -0,0 +1,23 @@
+#include "attack_map.hpp"
+
+void AttackMap::initAttackMap()
+{
+    initPawnAttacks();
+    initKnightAttacks();
+    initKingAttacks();
+    initRayTables();
+}
+
+/*********************** PRIVATE HELPERS ********************************/
+
+void AttackMap::initPawnAttacks()
+{}
+
+void AttackMap::initKnightAttacks()
+{}
+
+void AttackMap::initKingAttacks()
+{}
+
+void AttackMap::initRayTables()
+{}
