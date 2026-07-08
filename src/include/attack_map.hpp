4ecc3b2c7384d11b9a0b3f92055cc360907547eb
Youngwoo Chang <youngwc7@uci.edu>
2026-07-08 21:33:59 +0900

    precomputed attack map computation files.

--- src/include/attack_map.hpp
+++ src/include/attack_map.hpp
@@ -10,19 +10,19 @@
 
 struct AttackMap
 {
-    static uint64_t pawnAttackMap[NUM_COLORS][NUM_SQUARES];
-    static uint64_t knightAttackMap[NUM_SQUARES];
-    static uint64_t kingAttackMap[NUM_SQUARES];
+    inline static uint64_t pawnAttackMap[NUM_COLORS][NUM_SQUARES] = {};
+    inline static uint64_t knightAttackMap[NUM_SQUARES] = {};
+    inline static uint64_t kingAttackMap[NUM_SQUARES] = {};
 
-    static uint64_t rayN[NUM_SQUARES];
-    static uint64_t rayS[NUM_SQUARES];
-    static uint64_t rayE[NUM_SQUARES];
-    static uint64_t rayW[NUM_SQUARES];
+    inline static uint64_t rayN[NUM_SQUARES] = {};
+    inline static uint64_t rayS[NUM_SQUARES] = {};
+    inline static uint64_t rayE[NUM_SQUARES] = {};
+    inline static uint64_t rayW[NUM_SQUARES] = {};
 
-    static uint64_t rayNE[NUM_SQUARES];
-    static uint64_t rayNW[NUM_SQUARES];
-    static uint64_t raySE[NUM_SQUARES];
-    static uint64_t raySW[NUM_SQUARES];
+    inline static uint64_t rayNE[NUM_SQUARES] = {};
+    inline static uint64_t rayNW[NUM_SQUARES] = {};
+    inline static uint64_t raySE[NUM_SQUARES] = {};
+    inline static uint64_t raySW[NUM_SQUARES] = {};
 
     /** 
      * @brief acts as the constructor for this struct. Initializes members.
